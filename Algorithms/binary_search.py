09559156fb1ce94df2f043bddd49513855e2158d
camelliagh <camellia.gh@gmail.com>
2025-12-11 12:00:13 -0800

    Refactor binary search implementation to improve clarity and efficiency; add requirements.txt for Facebook API dependencies

--- Algorithms/binary_search.py
+++ Algorithms/binary_search.py
@@ -1,41 +1,38 @@
-def binary_search_recursive(n, sorted_array, left=0, right=None):
+def binary_search(n, array):  # [0,1,2,3,4]
+    left = 0
+    right = len(array) - 1
+    while right - left >= 0:
+        mid = (left + right) // 2
+        if array[mid] == n:
+            return mid
+        if array[mid] > n:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
+
+
+def binary_search_recursive(n, array, left=0, right=None):
 
     if right is None:
-        right = len(sorted_array) - 1
-
+        right = len(array) - 1
     if left > right:
         return -1
 
     middle = (right + left) // 2
-
-    if n == sorted_array[middle]:
+    if array[middle] == n:
         return middle
-    elif n > sorted_array[middle]:
-        return binary_search_recursive(n, sorted_array, middle + 1, right)
+
+    if array[middle] > n:
+        return binary_search_recursive(n, array, left, middle - 1)
     else:
-        return binary_search_recursive(n, sorted_array, left, middle - 1)
+        return binary_search_recursive(n, array, middle + 1, right)
 
 
-def binary_search(n, sorted_array):
-    low = 0
-    high = len(sorted_array)
-
-    while high - low > 1:
-        middle = (low + high) // 2
-
-        if n > sorted_array[middle]:
-            low = middle
-        else:
-            high = middle
-    return low if sorted_array[low] == n else None
-
-
-if __name__ == '__main__':
-    # print(binary_search_recursive(10, [2, 3, 4, 10, 16, 20, 35, 49, 55]))
-    # print(binary_search_recursive(1, [1]))
-    # print(binary_search_recursive(1, []))
-
-    # print(binary_search(10, [2, 3, 4, 10, 16, 20, 35, 49, 55]))
-    # print(binary_search(1, [1]))
-    # print(binary_search(1, []))
-    print(binary_search_recursive(9, [7, 8, 9, 2, 3, 4],3,-6))
+# print(binary_search(1, [0, 1, 2, 3, 4]))
+# print(binary_search(4, [0, 1, 2, 3, 4]))
+print(binary_search_recursive(4, [0, 1, 2, 3, 4]))
+print(binary_search_recursive(0, [0, 1, 2, 3, 4]))
+print(binary_search_recursive(3, [0, 1, 2, 3, 4]))
+print(binary_search_recursive(1, [0, 1], 0, 1))
+print(binary_search_recursive(0, [0]))
